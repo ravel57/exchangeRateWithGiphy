afab965d601823f4fdb4f630e7e9da5d13686b7f
petr <petr>
2022-06-05 01:18:48 +0300

    first commit

---
+++ src/main/java/ru/ravel/exchangeRateWithGiphy/ExchangeRateWithGiphyApplication.java
@@ -0,0 +1,13 @@
+package ru.ravel.exchangeRateWithGiphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateWithGiphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateWithGiphyApplication.class, args);
+	}
+
+}
